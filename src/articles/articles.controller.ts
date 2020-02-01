065d57c2fc06f695649675807423bc9f8eedf26a
tongmaster <basfers@gmail.com>
2020-02-01 11:42:40 +0700

    create update delete patch

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,21 +1,47 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller('articles')
 export class ArticlesController {
     
     artitcles = [
-        {id : '1', title : 'Title' , body : 'Body#1'},
-        {id : '2', title : 'Title', body : 'Body#2'}
+        {id : '1', title : 'Title#1' , body : 'Body#1'},
+        {id : '2', title : 'Title#2', body : 'Body#2'},
+        {id : '3', title : 'Title#3', body : 'Body#3'},
+        {id : '4', title : 'Title#4', body : 'Body#4'}
         
     ]
     
     @Get('/')
     findAll(){
-        return [{id : 1, title : 'Title'}]
+        return this.artitcles
     }
 
     @Get(':id')
     findOne(@Param('id') id){
         return this.artitcles.find(articles => articles.id === id)
+    }
+
+    @Post('/')
+    create(@Body() input ){
+        const article = {...input , id : +new Date()}
+        this.artitcles.push(article)
+        return article
+    }
+    
+    @Patch(':id')
+    update(@Param('id') id , @Body() body){
+        const article = this.artitcles.find(article => article.id ===id)
+        // article.body
+        for(let key in body){
+            article[key] = body[key]
+        }
+        return article
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    // @HttpCode(204)
+    delete(@Param('id') id){
+        this.artitcles = this.artitcles.filter(article => article.id !== id)
     }
 }
